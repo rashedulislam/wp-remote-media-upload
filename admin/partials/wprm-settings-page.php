827471963991d5d9cef1c84675c69ba36750ebd4
Md Rashedul Islam <rashedulislam.ruet@gmail.com>
2021-05-21 00:44:49 +0600

    fixed textarea issue

--- admin/partials/wprm-settings-page.php
+++ admin/partials/wprm-settings-page.php
@@ -12,12 +12,12 @@
 <div class="wprmu-main-wrap wrap">
 
    <h1><?php _e( 'WP Remote Media Upload Settings', 'wp_remote_media_upload' ); ?></h1>
-   <form method="post" action="<?php echo get_bloginfo( 'wpurl' ) . '/wp-admin/tools.php?page=transient-options' ?>">
+   <form method="post" action="<?php echo get_bloginfo( 'wpurl' ) . '/wp-admin/upload.php?page=wprm-options' ?>">
         <table class="form-table">
             <tr>
-                <th scope="row"><label for="upgrade_optimize"><?php echo ucwords( __( 'Image URLS', 'artiss-transient-cleaner' ) ); ?></label></th>
+                <th scope="row"><label for="image_urls"><?php echo ucwords( __( 'IMAGE URLS:', 'artiss-transient-cleaner' ) ); ?><?php echo _e( '<br>(Enter Urls separated by "," or "|")', 'wp_remote_media_upload' ); ?></label></th>
 
-                <td><input type="textarea" name="image_urls" cols="25" placeholder="Enter Image Urls" required="required" rows="8" ></textarea><?php echo _e( '  Enter Urls separated by "," or "|"', 'wp_remote_media_upload' ); ?></td>
+                <td><textarea name="image_urls" id="image_urls" cols="100" placeholder="Enter Image Urls" required="required" rows="8" ></textarea></td>
             </tr>
         </table>
         <?php wp_nonce_field( 'wp-remote-media-upload-options', 'wp_remote_media_upload_options_nonce', true, true ); ?>
